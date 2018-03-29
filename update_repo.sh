32a5af96cc316d4982c471087b7e0ce337f98a2e
Takao <tshibamot42@students.tntech.edu>
2018-03-29 15:14:48 -0500

    small change

--- update_repo.sh
+++ update_repo.sh
@@ -14,7 +14,7 @@ git checkout master
 mv .git /tmp/primer-server-rest-api-tmp/
 mv venv /tmp/primer-server-rest-api-tmp/
 rm -fR ./* ./.*
-cp -a /tmp/primer-server-rest-api-tmp/. ./
+mv -a /tmp/primer-server-rest-api-tmp/. ./
 git add --all .
 git commit -m "from dev"
 git push origin master
