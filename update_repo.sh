6eb9ee244b3085e1bbbc0f733239415cae33e871
Takao <tshibamot42@students.tntech.edu>
2018-03-29 14:49:00 -0500

    from dev

---
+++ update_repo.sh
@@ -0,0 +1,20 @@
+# make sure to push dev first
+git checkout dev
+git add --all .
+git commit -m "small change"
+git push origin dev
+
+# copy all dev except .git and venv to /tmp/primer-server-rest-api-tmp/
+rm -fR /tmp/primer-server-rest-api-tmp
+mkdir /tmp/primer-server-rest-api-tmp
+rsync -aP . /tmp/primer-server-rest-api-tmp/ --exclude=.git --exclude=venv --exclude=updateRepo.sh
+
+# copy back all files in /tmp/primer-server-rest-api-tmp/ to master
+git checkout master
+cp -a /tmp/primer-server-rest-api-tmp/. ./
+git add --all .
+git commit -m "from dev"
+git push origin master
+
+# come back to dev
+git checkout dev
