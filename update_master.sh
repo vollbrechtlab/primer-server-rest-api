0aab6392f5f8a5e82c7fe50fed9c7e7dd00d7875
Takao Shibamoto <takao8@gmx.com>
2018-04-24 16:39:47 -0500

    small change

--- update_master.sh
+++ update_master.sh
@@ -4,17 +4,26 @@ git add --all .
 git commit -m "small change"
 git push origin dev
 
-# copy all dev except .git and venv to /tmp/primer-server-rest-api-tmp/
+# cleanup current tmp
 rm -fR /tmp/primer-server-rest-api-tmp
 mkdir /tmp/primer-server-rest-api-tmp
-rsync -aP . /tmp/primer-server-rest-api-tmp/ --exclude=.git --exclude=venv --exclude=update_repo.sh
 
-# copy back all files in /tmp/primer-server-rest-api-tmp/ to master
+# copy all nessesary files to /tmp/primer-server-rest-api-tmp/
+cp {.gitignore,LICENSE,README.md,deploy_repo.py,pdaft.conf,requirements.txt,server.py,start_server.sh,stop_server.sh,supported_genomes.json,task_thread.py,utilities.py,version.py} /tmp/primer-server-rest-api-tmp/
+cp -R primerDAFT /tmp/primer-server-rest-api-tmp/
+cp -R tests /tmp/primer-server-rest-api-tmp/
+
+# go to master branch
 git checkout master
 git pull
-mv .git /tmp/primer-server-rest-api-tmp/
-mv venv /tmp/primer-server-rest-api-tmp/
-rm -fR ./* ./.*
+
+# remove nessasary files
+rm .angular-cli.json .editorconfig .gitignore README.md deploy.js karma.conf.js package-lock.json package.json protractor.conf.js tsconfig.json tslint.json
+rm -R src
+rm -R e2e
+rm -R other_data
+
+# copy back all files in /tmp/primer-server-rest-api-tmp/ to master and push
 cp -a /tmp/primer-server-rest-api-tmp/. ./
 git add --all .
 git commit -m "$1"
